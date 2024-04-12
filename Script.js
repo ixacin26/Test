735a54af4476ffea044264b7815edbfd63dc8caa
ixacin26 <ixacin26@gmail.com>
2024-04-12 09:39:12 +0200

    edited some days

--- Script.js
+++ Script.js
@@ -18,7 +18,7 @@ var x = setInterval(function () {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML =
+  document.getElementById("timeToGo").innerHTML =
     "noch " +
     days +
     " Tage " +
@@ -32,7 +32,7 @@ var x = setInterval(function () {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "Wir sind unterwegs!";
+    document.getElementById("timeToGo").innerHTML = "Wir sind unterwegs!";
   }
 });
 // ************************************************************
